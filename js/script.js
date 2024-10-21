b7bcf7c59e0e5f197095d843c3e5e90a4d24e3a7
Farhadur Rahman <farhadurrahman.help4@gmail.com>
2024-10-21 19:50:25 +0600

    Better Responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-const hamburger=document.getElementById("hamburger");const navBar=document.querySelector(".nav_bar");hamburger.addEventListener("click",()=>{navBar.classList.toggle("active")});
+const menuToggle = document.querySelector('.menu_toggle');
+const navigation = document.querySelector('.main_navigation');
+
+menuToggle.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+});
