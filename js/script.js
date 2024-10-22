193554cb2d4d2d366736daa8ef1938a2b9fcfa53
Farhadur Rahman <farhadurrahman.help4@gmail.com>
2024-10-22 16:37:54 +0600

    Skill Section is Better_

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ const navigation = document.querySelector('.main_navigation');
 menuToggle.addEventListener('click', () => {
     navigation.classList.toggle('active');
 });
+
+// Back to Top 
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    const backToTopBtn = document.getElementById("backToTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block"; 
+    } else {
+        backToTopBtn.style.display = "none"; 
+    }
+}
+
+document.getElementById("backToTop").addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
